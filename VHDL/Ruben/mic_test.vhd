23af87e27bec51f2e916fed46d21164af82b9d63
DeliveryDroid <30692307+DeliveryDroid@users.noreply.github.com>
2023-06-13 14:00:36 +0200

    Add files via upload

--- VHDL/Ruben/mic_test.vhd
+++ VHDL/Ruben/mic_test.vhd
@@ -9,6 +9,7 @@ entity mic_test is
            i_FPGAclk : in std_logic; -- internal FPGA clock
            i_mic : in STD_LOGIC; -- microphone output
            o_mic_clk: out STD_LOGIC; -- mic clk
+           o_S_clk: out STD_LOGIC; -- clk for reading sensor values
            o_mic : out std_logic); -- monitor this pin
 end mic_test;
 
@@ -16,6 +17,8 @@ architecture Behavioral of mic_test is
 
 signal PDMclk_counter : unsigned(c_PDMclkcounterbits-1 downto 0) := (others => '0');
 signal PDMclk: std_logic := '1'; -- assumes PDM microphone setting to be low
+signal Sclkdelay_counter : unsigned(c_CICclockdivbits-1 downto 0) := (others => '0');
+signal Fil_i_S_clk : STD_LOGIC := '0';
 
 begin
 
@@ -32,6 +35,22 @@ begin
     end process PDMclkdivproc;
     o_mic_clk <= PDMclk; 
     o_mic <= i_mic;
+    
+    Sclkdelayproc:process(i_FPGAclk) -- A delay for the filter clock to guarantee valid data
+        begin
+        if rising_edge(i_FPGAclk) then
+            if (PDMclk = '0') then -- assumes PDM select to be low
+                if (Sclkdelay_counter = c_Sclkdelay_countermax) then
+                    Fil_i_S_clk <= '1';
+                else
+                    Sclkdelay_counter <= Sclkdelay_counter + 1;
+                end if;
+            else
+                Fil_i_S_clk <= '0';
+                Sclkdelay_counter <= (others => '0');
+            end if;
+        end if;
+    end process Sclkdelayproc;
 
 end Behavioral;
 
